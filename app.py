2685d4acd55e28c6fa65472cc44eb295a1f03a65
YuriPHB <yurigomesjunior@gmail.com>
2022-05-28 16:47:52 -0300

    Add mecanismo de saque saldo inicial 0

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ print("Bem vindo ao Banco Python")
 data = datetime.date.today().strftime("%d/%m/%Y")
 
 print(f"Data: {data}")
+
+valor = 0
+saque = input("Digite Valor: ")
+valor = saque
+print(valor)
